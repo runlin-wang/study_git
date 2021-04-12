851a2285aa8da486846cc0b67e56a12920591e97
Leo <alin.run@foxmail.com>
2021-04-12 16:39:23 +0800

    feat(*): update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # File              : commit.sh
 # Author            : Leo <alin.run@foxmail.com>
-# Date              : 20.03.2021
-# Last Modified Date: 02.04.2021
+# Date              : 2021-03-20
+# Last Modified Date: 2021-04-12
 # Last Modified By  : Leo <alin.run@foxmail.com>
 
-cd ~/github.com/000_leo/study_git
+cd ~/github.com/study_git
 
 # write some text
 fortune > last_msg
@@ -25,3 +25,16 @@ git commit -m "$msg"
 
 git push
 
+
+# end
+
+  cat <<'EOF'
+
+               __                                          _ __ 
+  ____ ___  __/ /_____     _________  ____ ___  ____ ___  (_) /_
+ / __ `/ / / / __/ __ \   / ___/ __ \/ __ `__ \/ __ `__ \/ / __/
+/ /_/ / /_/ / /_/ /_/ /  / /__/ /_/ / / / / / / / / / / / / /_  
+\__,_/\__,_/\__/\____/   \___/\____/_/ /_/ /_/_/ /_/ /_/_/\__/   .... is normal running.
+                                                                
+
+EOF
